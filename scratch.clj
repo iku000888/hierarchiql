f089820f26572fd1e4b675f727647276763574f9
Ikuru K <kanumaiku@gmail.com>
2018-12-17 01:12:58 +0900

    pull-1 on hetero map

--- scratch.clj
+++ scratch.clj
@@ -9,6 +9,7 @@
             :dbname "clojure_test_h2"})
 
 (j/execute! h2-db (j/drop-table-ddl :string))
+(j/execute! h2-db (j/drop-table-ddl :int))
 (j/execute! h2-db (j/drop-table-ddl :entities))
 (j/execute! h2-db (j/drop-table-ddl :transactions))
 
@@ -18,6 +19,13 @@
   :string
   [[:id :int "PRIMARY KEY AUTO_INCREMENT"]
    [:value :varchar "unique not null"]]))
+
+(j/execute!
+ h2-db
+ (j/create-table-ddl
+  :int
+  [[:id :int "PRIMARY KEY AUTO_INCREMENT"]
+   [:value :int "unique not null"]]))
 
 (j/execute!
  h2-db
@@ -38,7 +46,8 @@
   [[:id :int "PRIMARY KEY AUTO_INCREMENT"]]))
 
 (defn v->table [v]
-  (cond (string? v) "string"))
+  (cond (string? v) "string"
+        (integer? v) "int"))
 
 (defn transact [db vals]
   (let [transaction-id (:id (first (j/insert! h2-db :transactions {})))]
@@ -61,34 +70,57 @@
          :entities
          {:attribute_ns (namespace k)
           :attribute_name (name k)
-          :value_type "string"
+          :value_type (v->table v)
           :value_id v-id
           :transaction_id transaction-id})))))
 
-(defn pull-1 [db id]
-  (let [{:keys [id attribute_ns attribute_name
-                value_type value_id t]}
+(defn find-entity-1 [db input-id]
+  (-> (hp/select :*)
+      (hp/from :entities)
+      (hp/where [:= :id input-id])
+      h/format
+      (->> (j/query db))
+      first))
+
+(defn pull-1 [db input-id]
+  (let [{:keys [transaction_id]} (find-entity-1 db input-id)
+        siblings (-> (hp/select :*)
+                     (hp/from :entities)
+                     (hp/where [:= :transaction_id transaction_id])
+                     h/format
+                     (->> (j/query db)))]
+    (->> siblings
+         (map (fn [{:keys [id attribute_ns attribute_name
+                           value_type value_id t transaction_id]}]
+                {(keyword attribute_ns attribute_name)
+                 (-> (hp/select :value)
+                     (hp/from (keyword value_type))
+                     (hp/where [:= :id value_id])
+                     h/format
+                     (->> (j/query db))
+                     first
+                     :value)}))
+         (into {}))))
+
+(defn find [db attr v]
+  (let [{:keys [id value]}
         (-> (hp/select :*)
-            (hp/from :entities)
-            (hp/where [:= :id id])
+            (hp/from (keyword (v->table v)))
+            (hp/where [:= :value v])
             h/format
             (->> (j/query db))
             first)]
-    {(keyword attribute_ns attribute_name)
-     (-> (hp/select :value)
-         (hp/from (keyword (v->table value_type)))
-         (hp/where [:= :id value_id])
-         h/format
-         (->> (j/query db))
-         first
-         :value)}))
+    (-> (hp/select :*)
+        (hp/from :entities)
+        (hp/where [:= :value_id id])
+        h/format
+        (->> (j/query db)))))
 
 (comment
-  (transact h2-db {:foo/bar "foo"})
+  (transact h2-db {:foo/bar "foo"
+                   :foo/int 3})
   (pull-1 h2-db 1)
-
+  (find h2-db :foo/bar "foo")
   (j/query h2-db "select * from entities")
   (j/query h2-db "select * from string")
-  (let [{:keys [entity value]}
-        (:entity (first (j/query h2-db "select * from string where value = 'foo'")))]
-    (j/query "select * from")))
+  (j/query h2-db "select * from int"))
