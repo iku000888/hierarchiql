6f07159f730f8dba96afa3a70eb25c2748ab4ff4
Ikuru K <kanumaiku@gmail.com>
2018-12-17 00:46:33 +0900

    Hackhackhack

--- scratch.clj
+++ scratch.clj
@@ -1,21 +1,23 @@
 ;; This buffer is for Clojure experiments and evaluation.
 ;; Press C-j to evaluate the last expression.
 
-
+(require '[honeysql.format :as h])
+(require '[honeysql.helpers :as hp])
 (require '[clojure.java.jdbc :as j])
 
 (def h2-db {:dbtype "h2:mem"
             :dbname "clojure_test_h2"})
 
-(j/execute! h2-db (j/drop-table-ddl :strings))
+(j/execute! h2-db (j/drop-table-ddl :string))
 (j/execute! h2-db (j/drop-table-ddl :entities))
+(j/execute! h2-db (j/drop-table-ddl :transactions))
 
 (j/execute!
  h2-db
  (j/create-table-ddl
-  :strings
-  [[:entity :int "PRIMARY KEY AUTO_INCREMENT"]
-   [:value :varchar "not null"]]))
+  :string
+  [[:id :int "PRIMARY KEY AUTO_INCREMENT"]
+   [:value :varchar "unique not null"]]))
 
 (j/execute!
  h2-db
@@ -24,29 +26,69 @@
   [[:id :int "PRIMARY KEY AUTO_INCREMENT"]
    [:attribute_ns :varchar "not null"]
    [:attribute_name :varchar "not null"]
-   [:type :varchar "not null"]
-   [:entity :int]
-   [:t :int "not null AUTO_INCREMENT"]]))
+   [:value_type :varchar "not null"]
+   [:value_id :int]
+   [:t :int "not null AUTO_INCREMENT"]
+   [:transaction_id :int "not null"]]))
 
-
-(j/insert!
+(j/execute!
  h2-db
- :entities
- {:attribute_ns "foo"
-  :attribute_name "bar"
-  :type "strings"
-  :entity 1})
+ (j/create-table-ddl
+  :transactions
+  [[:id :int "PRIMARY KEY AUTO_INCREMENT"]]))
 
+(defn v->table [v]
+  (cond (string? v) "string"))
 
 (defn transact [db vals]
-  )
+  (let [transaction-id (:id (first (j/insert! h2-db :transactions {})))]
+    (doseq [[k v] vals]
+      (let [v-type (v->table v)
+            v-id (if-let [v-id (-> (hp/select :*)
+                                   (hp/from (keyword v-type))
+                                   (hp/where [:= :value v])
+                                   h/format
+                                   (->> (j/query db))
+                                   first
+                                   :id)]
+                   v-id
+                   (-> (j/insert! db (keyword v-type)
+                                  {:value v})
+                       first
+                       :id))]
+        (j/insert!
+         h2-db
+         :entities
+         {:attribute_ns (namespace k)
+          :attribute_name (name k)
+          :value_type "string"
+          :value_id v-id
+          :transaction_id transaction-id})))))
 
-(transact h2-db {:foo/bar "foo"})
-(j/query h2-db "select * from entities")
-(j/query h2-db "select * from strings")
+(defn pull-1 [db id]
+  (let [{:keys [id attribute_ns attribute_name
+                value_type value_id t]}
+        (-> (hp/select :*)
+            (hp/from :entities)
+            (hp/where [:= :id id])
+            h/format
+            (->> (j/query db))
+            first)]
+    {(keyword attribute_ns attribute_name)
+     (-> (hp/select :value)
+         (hp/from (keyword (v->table value_type)))
+         (hp/where [:= :id value_id])
+         h/format
+         (->> (j/query db))
+         first
+         :value)}))
 
+(comment
+  (transact h2-db {:foo/bar "foo"})
+  (pull-1 h2-db 1)
 
-
-(j/execute! h2-db "create table string ")
-(j/insert! h2-db :foo {})
-(j/query h2-db "select * from foo")
+  (j/query h2-db "select * from entities")
+  (j/query h2-db "select * from string")
+  (let [{:keys [entity value]}
+        (:entity (first (j/query h2-db "select * from string where value = 'foo'")))]
+    (j/query "select * from")))
